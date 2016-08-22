3b4636b3bb43da514a410b333d23f63b6c7e489e
Martin Bless <martin@mbless.de>
2016-08-22 15:32:12 +0200

    Add lockfile removal

--- 50-Remove-lock/run_01.py
+++ 50-Remove-lock/run_01.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 # ==================================================
 # open
 # --------------------------------------------------
 
 from __future__ import print_function
-import tct
 import os
+import tct
 import sys
 
 params = tct.readjson(sys.argv[1])
@@ -22,9 +23,8 @@ exitcode = CONTINUE = 0
 # define
 # --------------------------------------------------
 
-lockfile = ''
-lockfile_planned = ''
-lockfile_create_logstamp = ''
+lockfile_removed = ''
+lockfile_remove_logstamp = ''
 
 # ==================================================
 # Get and check required milestone(s)
@@ -35,56 +35,33 @@ def milestones_get(name, default=None):
     loglist.append((name, result))
     return result
 
-def params_get(name, default=None):
-    result = params.get(name, default)
-    loglist.append((name, result))
-    return result
-
 if exitcode == CONTINUE:
-    loglist.append(('CHECK PARAMS'))
-    toolchain_name = params_get('toolchain_name')
-    toolchain_temp_home = params_get('toolchain_temp_home')
-    if not (toolchain_name and toolchain_temp_home):
-        exitcode = 2
-
-if exitcode == CONTINUE:
-    lockfile_name = tct.deepget(facts, 'tctconfig', toolchain_name, 'lockfile_name')
-    loglist.append(('lockfile_name', lockfile_name))
-    if not lockfile_name:
-        exitcode = 2
-
-if exitcode == CONTINUE:
-    lockfile_planned = os.path.join(toolchain_temp_home, lockfile_name)
-    loglist.append(('lockfile_planned', lockfile_planned))
-
-if exitcode == CONTINUE:
-    loglist.append('PARAMS are ok')
-else:
-    loglist.append('PROBLEMS with params')
+    lockfile = milestones_get('lockfiles')
+    if not (lockfile):
+        CONTINUE = -1
 
 # ==================================================
 # work
 # --------------------------------------------------
 
 if exitcode == CONTINUE:
-    if os.path.exists(lockfile_planned):
-        loglist.append('lockfile already exists!')
-        exitcode = 2
-
-if exitcode == CONTINUE:
-    tct.writejson(facts, lockfile_planned)
-    lockfile_create_logstamp = tct.logstamp_finegrained()
-    lockfile = lockfile_planned
+    if os.path.isfile(lockfile):
+        os.remove(lockfile)
+        lockfile_removed = lockfile
+        lockfile = ''
+        lockfile_remove_logstamp = tct.logstamp_finegrained()
 
 # ==================================================
 # Set MILESTONE
 # --------------------------------------------------
 
 if exitcode == CONTINUE:
-    result['MILESTONES'].append({
-        'lockfile': lockfile,
-        'lockfile_create_logstamp': lockfile_create_logstamp,
-    })
+    if lockfile_removed:
+        result['MILESTONES'].append({
+            'lockfile': lockfile,
+            'lockfile_removed': lockfile_removed,
+            'lockfile_remove_logstamp': lockfile_remove_logstamp,
+        })
 
 # ==================================================
 # save result
