f2971cce43f562aba92d4341b211d29a01b8f014
Martin Bless <martin.bless@mbless.de>
2017-09-04 10:13:47 +0200

    [TASK] Improve setting of initial params

--- 10-Toolchain-actions/run_01-Start-with-everything.py
+++ 10-Toolchain-actions/run_01-Start-with-everything.py
@@ -47,13 +47,18 @@ def firstNotNone(*args):
     else:
         return None
 
-def findRunParameter(key, default=None, D=None):
+def findRunParameter(key, default=None, D=None, fconv=None):
     result = firstNotNone(
         deepget(milestones, key, default=None),
         deepget(facts, 'run_command', key, default=None),
+        # too bad that 'jobfile_data' isn't available at this point
+        deepget(milestones, 'jobfile_data', 'tctconfig', key, default=None),
         deepget(facts, 'tctconfig', configset, key, default=None),
         default,
         None)
+    # function convert
+    if fconv is not None:
+        result = fconv(result)
     if type(D) == type({}):
         D[key] = result
     return result
@@ -69,8 +74,11 @@ import time
 # configset = deepget(facts, 'run_command', 'configset', default='default')
 ATNM['active_section'] = (milestones.get('active_section')
                   or deepget(facts, 'active_section', default='default'))
+
+# too bad we used both names:
 configset = ATNM['active_section']
 ATNM['configset'] = configset
+
 time_started_at_unixtime = time.time()
 ATNM['time_started_at_unixtime'] = time_started_at_unixtime
 time_started_at = tct.logstamp_finegrained(unixtime=time_started_at_unixtime, fmt='%Y-%m-%d %H:%M:%S %f')
@@ -81,17 +89,17 @@ ATNM['time_started_at'] = time_started_at
 # Set initial values (2)
 # --------------------------------------------------
 
-debug_always_make_milestones_snapshot = findRunParameter('debug_always_make_milestones_snapshot', 1, ATNM)
-force_rebuild_needed = findRunParameter('force_rebuild_needed', 1, ATNM)
-make_html = findRunParameter('make_html', 1, ATNM)
-make_latex = findRunParameter('make_latex', 1, ATNM)
-make_pdf = findRunParameter('make_pdf', 1, ATNM)
-make_singlehtml = findRunParameter('make_singlehtml', 1, ATNM)
+debug_always_make_milestones_snapshot = findRunParameter('debug_always_make_milestones_snapshot', 1, ATNM, int)
+force_rebuild_needed = findRunParameter('force_rebuild_needed', 1, ATNM, int)
+make_html = findRunParameter('make_html', 1, ATNM, int)
+make_latex = findRunParameter('make_latex', 1, ATNM, int)
+make_pdf = findRunParameter('make_pdf', 1, ATNM, int)
+make_singlehtml = findRunParameter('make_singlehtml', 1, ATNM, int)
 makedir = findRunParameter('makedir', None, ATNM)
-rebuild_needed = findRunParameter('rebuild_needed', 1, ATNM)
-replace_static_in_html = findRunParameter('replace_static_in_html', 0, ATNM)
+rebuild_needed = findRunParameter('rebuild_needed', 1, ATNM, int)
+replace_static_in_html = findRunParameter('replace_static_in_html', 0, ATNM, int)
 smtp_host = findRunParameter('smtp_host', 'None', ATNM)
-talk = findRunParameter('talk', 0, ATNM)
+talk = findRunParameter('talk', 0, ATNM, int)
 
 
 # ==================================================
@@ -106,22 +114,12 @@ if exitcode == CONTINUE:
 if exitcode == CONTINUE:
     loglist.append('PARAMS are ok')
 else:
-    loglist.append('PROBLEM with params')
-
-if CONTINUE != 0:
-    loglist.append({'CONTINUE': CONTINUE})
-    loglist.append('NOTHING to do')
+    loglist.append('Bad PARAMS or nothing to do')
 
 
 # ==================================================
 # work
 # --------------------------------------------------
-
-if exitcode == CONTINUE:
-    talk_builtin = 1
-    talk_run_command = tct.deepget(facts, 'run_command', 'talk')
-    talk_tctconfig = tct.deepget(facts, 'tctconfig', configset, 'talk')
-    talk = int(talk_run_command or talk_tctconfig or talk_builtin)
 
 if talk > 1:
     print('# --------', toolchain_name, time_started_at)
@@ -131,7 +129,7 @@ if talk > 1:
 # Set MILESTONE
 # --------------------------------------------------
 
-if 'always':
+if 'ATNM - All The New Milestones':
     result['MILESTONES'].append(ATNM)
 
 
