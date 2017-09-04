41a0c23713e1dd39c1d4a3845a65d149f2536da7
Martin Bless <martin.bless@mbless.de>
2017-09-04 10:25:57 +0200

    [TASK] Improve 'fetch ter_extension'

--- 14-Provide-the-project/12-Fetch-ter-extension/run_Fetch-ter-extension.py
+++ 14-Provide-the-project/12-Fetch-ter-extension/run_Fetch-ter-extension.py
@@ -7,6 +7,13 @@
 from __future__ import print_function
 import tct
 import sys
+#
+import codecs
+import os
+import re
+import shutil
+import subprocess
+
 
 params = tct.readjson(sys.argv[1])
 binabspath = sys.argv[2]
@@ -45,6 +52,7 @@ def lookup(D, *keys, **kwdargs):
 # --------------------------------------------------
 
 buildsettings = None
+buildsettings_builddir_new = None
 buildsettings_changed = False
 extension_file = ''
 extension_file_abspath = ''
@@ -53,10 +61,10 @@ gitbranch = ''
 gitdir = ''
 gitdir_unpacked = ''
 giturl = ''
+relative_part_of_builddir_new = None
 ter_extkey = ''
 ter_extversion = ''
 xeq_name_cnt = 0
-
 
 
 # ==================================================
@@ -69,43 +77,47 @@ if exitcode == CONTINUE:
     configset = lookup(milestones, 'configset')
     buildsettings = lookup(milestones, 'buildsettings')
     if not (configset and buildsettings):
-        exitcode = 22
+        CONTINUE = -2
 
 if exitcode == CONTINUE:
+    builddir = lookup(milestones, 'buildsettings', 'builddir', default='')
+    buildsettings_builddir = lookup(milestones, 'buildsettings_builddir',
+                                    default='')
+    relative_part_of_builddir = lookup(milestones, 'relative_part_of_builddir',
+                                    default='')
 
-    extensions_rootfolder = lookup(facts, 'tctconfig', configset, 'extensions_rootfolder')
-    gitbranch = lookup(milestones, 'buildsettings', 'gitbranch')
-    gitdir = lookup(milestones, 'buildsettings', 'gitdir')
-    giturl = lookup(milestones, 'buildsettings', 'giturl')
-    ter_extkey = lookup(milestones, 'buildsettings', 'ter_extkey')
-    ter_extversion = lookup(milestones, 'buildsettings', 'ter_extversion')
-
+if exitcode == CONTINUE:
+    ter_extkey = buildsettings.get('ter_extkey', '')
+    ter_extversion = buildsettings.get('ter_extversion', '')
     if not (ter_extkey and ter_extversion):
         loglist.append('For a TER extension we need the key and the version.')
         CONTINUE = -2
 
 if exitcode == CONTINUE:
-    if not gitdir and not extensions_rootfolder:
+    extensions_rootfolder = lookup(facts, 'tctconfig', configset, 'extensions_rootfolder')
+    if not extensions_rootfolder:
+        loglist.append('We need a place to unpack to')
         CONTINUE = -2
+
+if exitcode == CONTINUE:
+    gitbranch = buildsettings.get('gitbranch', '')
+    giturl = buildsettings.get('giturl', '')
+    if giturl:
+        loglist.append('We can\'t have both: TER_EXTKEY and GITURL')
+        CONTINUE = -2
+
+#   gitdir = lookup(milestones, 'buildsettings', 'gitdir')
+
 
 if exitcode == CONTINUE:
     loglist.append('PARAMS are ok')
 else:
-    loglist.append('PROBLEM with required params')
-
-if CONTINUE != 0:
-    loglist.append({'CONTINUE': CONTINUE})
-    loglist.append('NOTHING to do')
+    loglist.append('Bad PARAMS or nothing to do')
 
 
 # ==================================================
 # work
 # --------------------------------------------------
-
-import codecs
-import os
-import shutil
-import subprocess
 
 if exitcode == CONTINUE:
 
@@ -121,8 +133,7 @@ if exitcode == CONTINUE:
         global xeq_name_cnt
         cmd = ' '.join(cmdlist)
         cmd_multiline = ' \\\n   '.join(cmdlist) + '\n'
-        exitcode, cmd, out, err = cmdline(cmd, cwd=workdir)
-        loglist.append({'exitcode': exitcode, 'cmd': cmd, 'out': out, 'err': err})
+
         xeq_name_cnt += 1
         filename_cmd = 'xeq-%s-%d-%s.txt' % (toolname_pure, xeq_name_cnt, 'cmd')
         filename_err = 'xeq-%s-%d-%s.txt' % (toolname_pure, xeq_name_cnt, 'err')
@@ -130,6 +141,9 @@ if exitcode == CONTINUE:
 
         with codecs.open(os.path.join(workdir, filename_cmd), 'w', 'utf-8') as f2:
             f2.write(cmd_multiline.decode('utf-8', 'replace'))
+
+        exitcode, cmd, out, err = cmdline(cmd, cwd=workdir)
+        loglist.append({'exitcode': exitcode, 'cmd': cmd, 'out': out, 'err': err})
 
         with codecs.open(os.path.join(workdir, filename_out), 'w', 'utf-8') as f2:
             f2.write(out.decode('utf-8', 'replace'))
@@ -176,6 +190,7 @@ if exitcode == CONTINUE:
         't3xutils.phar', 'extract', extension_file_abspath, gitdir_unpacked])
 
 if exitcode == CONTINUE:
+    xx_xx = re.compile('([a-z]{2}-[a-z]{2})|default')
     buildsettings['gitdir'] = gitdir_unpacked
     buildsettings_changed = True
 
@@ -186,6 +201,10 @@ if exitcode == CONTINUE:
 
 D = {}
 
+if buildsettings_builddir_new is not None:
+    # Change! Overwrite! According to TER extensions
+    D['buildsettings_builddir'] = buildsettings_builddir_new
+
 if extension_file:
     D['extension_file'] = extension_file
 
@@ -193,7 +212,12 @@ if extension_file_abspath:
     D['extension_file_abspath'] = extension_file_abspath
 
 if buildsettings_changed:
+    # Change! Overwrite! According to TER extensions
     D['buildsettings'] = buildsettings
+
+if relative_part_of_builddir_new:
+    # Change! Overwrite! According to TER extensions
+    D['relative_part_of_builddir'] = relative_part_of_builddir_new
 
 if D:
     result['MILESTONES'].append(D)
