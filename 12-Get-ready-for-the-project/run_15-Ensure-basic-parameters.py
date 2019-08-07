44c35bf0f5b5dfe6c40f2ad184d0d237d435fdde
Martin Bless <martin.bless@mbless.de>
2019-08-07 15:42:17 +0200

    Rewrite code, add 'resultdir' in run_15-Ensure-basic-parameters.py

--- 12-Get-ready-for-the-project/run_15-Ensure-basic-parameters.py
+++ 12-Get-ready-for-the-project/run_15-Ensure-basic-parameters.py
@@ -10,6 +10,13 @@ import tct
 import os
 import sys
 
+ospabsp = os.path.abspath
+ospe = os.path.exists
+ospisabs = os.path.isabs
+ospisdir = os.path.isdir
+ospj = os.path.join
+ospnormp = os.path.normpath
+
 params = tct.readjson(sys.argv[1])
 facts = tct.readjson(params['factsfile'])
 milestones = tct.readjson(params['milestonesfile'])
@@ -17,9 +24,11 @@ resultfile = params['resultfile']
 result = tct.readjson(resultfile)
 toolname = params['toolname']
 toolname_pure = params['toolname_pure']
+toolchain_name = facts['toolchain_name']
 workdir = params['workdir']
 loglist = result['loglist'] = result.get('loglist', [])
 exitcode = CONTINUE = 0
+initial_working_dir = facts['initial_working_dir']
 
 
 # ==================================================
@@ -34,10 +43,8 @@ if 0 or milestones.get('debug_always_make_milestones_snapshot'):
 # Helper functions
 # --------------------------------------------------
 
-deepget = tct.deepget
-
 def lookup(D, *keys, **kwdargs):
-    result = deepget(D, *keys, **kwdargs)
+    result = tct.deepget(D, *keys, **kwdargs)
     loglist.append((keys, result))
     return result
 
@@ -49,6 +56,7 @@ def lookup(D, *keys, **kwdargs):
 makedir = ''
 makedir_abspath = ''
 makedir_missing = ''
+resultdir = None
 talk = milestones.get('talk', 1)
 
 
@@ -58,15 +66,13 @@ talk = milestones.get('talk', 1)
 
 if exitcode == CONTINUE:
     loglist.append('CHECK PARAMS')
-    toolchain_name = lookup(facts, 'toolchain_name')
-    initial_working_dir = lookup(facts, 'initial_working_dir')
-    if not (toolchain_name and initial_working_dir):
-        exitcode = 22
-
-if exitcode == CONTINUE:
-    makedir = lookup(milestones, 'makedir')
+    makedir = lookup(milestones, 'makedir', default=None)
     if not makedir:
-        msg = 'Usage: tct run %s --config makedir MAKEDIR [--toolchain-help]' % toolchain_name
+        msg = (
+            "fatal error: parameter 'makedir' is missing\n'" 
+            'usage:\n'
+            '   tct run %s -c makedir MAKEDIR [--toolchain-help]\n' % toolchain_name
+        )
         loglist.append(msg)
         print(msg)
         exitcode = 22
@@ -99,6 +105,20 @@ if exitcode == CONTINUE:
         print('makedir:', os.path.split(makedir)[1])
         print('makedir_abspath:', makedir_abspath)
 
+if exitcode == CONTINUE:
+    resultdir = lookup(milestones, 'resultdir')
+    if resultdir:
+        if not os.path.isabs(resultdir):
+            resultdir = ospabsp(ospj(initial_working_dir, resultdir))
+        resultdir = os.path.normpath(resultdir)
+        if not os.path.isdir(resultdir):
+            msg = ("fatal error: directory '%s' (resultdir) does not exist." %
+                   resultdir)
+            loglist.append(msg)
+            print(msg)
+            exitcode = 90
+
+
 # ==================================================
 # Set MILESTONE
 # --------------------------------------------------
@@ -111,6 +131,9 @@ if makedir_abspath:
 
 if makedir_missing:
     result['MILESTONES'].append({'makedir_missing': makedir_missing})
+
+if resultdir:
+    result['MILESTONES'].append({'resultdir': resultdir})
 
 # ==================================================
 # save result
