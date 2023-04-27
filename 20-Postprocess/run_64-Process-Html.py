86ab878a6b5855a3bcd4a8223e8a963278dd76de
Martin Bless <martin.bless@mbless.de>
2023-04-27 16:32:30 +0200

    Update run_64-Process-Html.py

--- 20-Postprocess/run_64-Process-Html.py
+++ 20-Postprocess/run_64-Process-Html.py
@@ -226,24 +226,14 @@ def process_html_file(folder, relpath):
         # input:  div.literal-block-wrapper.docutils.container
         # output: div.literal-block-wrapper.docutils.du-container
         classes1 = elm.get("class", [])
-        classes2 = []
-        hits = 0
-        for v in classes1:
-            if "literal-block-wrapper" == v:
-                hits += 1
+        if "docutils" in classes1 and "container" in classes1:
+            classes2 = []
+            for v in classes1:
+                if v == "container":
+                    v = "du-container"
                 classes2.append(v)
-            elif "docutils" == v:
-                hits += 1
-                classes2.append(v)
-            elif "container" == v:
-                hits += 1
-                classes2.append("du-container")
-            else:
-                hits += 0
-                classes2.append(v)
-        if hits >= 3:
-            soup_modified = True
             elm["class"] = classes2
+            soup_modified = True
 
     for link in soup.find_all("a"):
         href = link.get("href")
